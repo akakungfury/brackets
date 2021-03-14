2f3b60d5c771e1359c035357ca6ee83362b6b05b
Aliaksandr Khudnitski <alekskhudnitskiy@gmail.com>
2021-03-14 21:33:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let availablePairs = bracketsConfig.map((el) => el.join(""));
+    let replacedStr = str;
+
+    for (let i = 0; i <= str.length / 2; i++) {
+        availablePairs.forEach((pair) => {
+            replacedStr = replacedStr.replace(pair, "");
+        });
+    }
+
+    return !replacedStr.length;
+};
